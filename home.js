3a741a81f0c91e0067dec8f8d908a2b0cb25a604
SamriddhiShah24 <77601757+SamriddhiShah24@users.noreply.github.com>
2023-01-23 20:24:24 +0530

    Delete home.js

--- home.js
+++
@@ -1,7 +0,0 @@
-var studentName = localStorage.getItem("studentName");
-var studentEmail = localStorage.getItem("studentEmail");;
-var studentGrade = localStorage.getItem("studentGrade");;
-
-
-console.log(studentGrade, studentEmail, studentName);
-
