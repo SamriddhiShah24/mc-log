dbd2dc03fc0caf7edbb0379903911fbf4155a649
SamriddhiShah24 <77601757+SamriddhiShah24@users.noreply.github.com>
2023-01-23 17:33:37 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+var studentName = localStorage.getItem("studentName");
+var studentEmail = localStorage.getItem("studentEmail");;
+var studentGrade = localStorage.getItem("studentGrade");;
+
+
+console.log(studentGrade, studentEmail, studentName);
+
---
+++ main.js
@@ -0,0 +1,19 @@
+var studentName = "";
+var studentEmail = "";
+var studentGrade = "";
+
+function login() {
+    studentName = document.getElementById("studentName").value;
+    studentEmail = document.getElementById("studentEmail").value;
+    studentGrade = document.getElementById("studentGrade").value;
+
+    localStorage.setItem("studentName", studentName);
+    localStorage.setItem("studentEmail", studentEmail);
+    localStorage.setItem("studentGrade", studentGrade);
+
+    console.log("logged in " + studentName + " " + studentEmail + " " + studentGrade);
+    window.location.href="/screen2/home.html";
+
+
+}
+
